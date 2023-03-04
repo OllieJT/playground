02da62c857128bcb20455d82dd7b1dc711bd129f
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2023-03-04 17:08:21 +0000

    Update env

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	import { base } from '$app/paths';
+
+	type Project = { path: string; label: string };
+	const projects: Project[] = [];
 </script>
 
 <ol>
-	<li>
-		<a href="{base}/omni-menu">Omni Menu</a>
-	</li>
+	{#each projects as { path, label }}
+		<li>
+			<a href="{base}/{path}">{label}</a>
+		</li>
+	{/each}
 </ol>
